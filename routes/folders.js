d14debbd7d8ecaac6743542b4c93ccb5ee70d707
Alexa Scott <arscott4792@gmail.com>
2018-03-22 10:01:22 -0700

    updated get all folders endpoint

---
+++ routes/folders.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+
+const Folder = require('../models/folder');
+const Note = require('../models/note');
+
+/* ========== GET/READ ALL ITEMS ========== */
+router.get('/folders', (req, res, next) => {
+  Folder.find()
+    .sort('name')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+
+
+
+
+module.exports = router;
