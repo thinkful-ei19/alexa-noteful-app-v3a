edbe841d167effa3cfe285afc03ff8cc6d4a9387
Alexa Scott <arscott4792@gmail.com>
2018-03-23 08:52:37 -0700

    updated put endpoint for tags

--- routes/tags.js
+++ routes/tags.js
@@ -67,6 +67,43 @@ router.post('/tags', (req, res, next) => {
     });
 });
 
+/* ========== PUT/UPDATE A SINGLE ITEM ========== */
+router.put('/tags/:id', (req, res, next) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  /***** Never trust users - validate input *****/
+  if (!name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+
+  const updatedItem = { name };
+  const options = { new: true};
+
+  Tag.findByIdAndUpdate(id, updatedItem, options)
+    .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('The tag name already exists');
+        err.status = 400;
+      }
+      next(err);
+    });
+});
 
 
 module.exports = router;
